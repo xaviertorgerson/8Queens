3c6822172934fa2755a2fd4661fbc2a20e5f3d91
Xavier Torgerson <xaviertorgerson@gmail.com>
2015-11-10 20:06:54 -0500

    Implement algorithm

--- FriendsCoupon.java
+++ FriendsCoupon.java
@@ -8,45 +8,82 @@ public class FriendsCoupon{
 	public static int numberOfUsers;
 
 	/**
-	*Checks if the given partial solution is complete
-	*@param partial Partial solution
-	*@return True if partial is a complete, valid solution
-	*/
+	 *Checks if the given partial solution is complete
+	 *@param partial Partial solution
+	 *@return True if partial is a complete, valid solution
+	 */
 	public static boolean isFullSolution(int[] partial){
 
 		return false;
 	}
-	
+
 	/**
-	*Checks if partial solution can become a complete solution
-	*@param partial Partial solution
-	*@return True if partial solution can NOT be extended to a complete solution
-	*/
+	 *Checks if partial solution can become a complete solution
+	 *@param partial Partial solution
+	 *@return True if partial solution can NOT be extended to a complete solution
+	 */
 	public static boolean reject(int[] partial){
-	
+		int i = 0;
+		while(i<friends[0].length){
+			if (partial[i+1] == 0){
+				
+				boolean goodCoupon = false;
+
+				for(int testCoupon = 1; !goodCoupon; testCoupon++) 
+				{
+					goodCoupon = true;
+					//Check all friends of i and their 
+					for(int k = 0; k < numberOfUsers; k++) {
+						if(friends[i][k] == 1) {
+							if(partial[k] == testCoupon) {
+								goodCoupon = false;
+								k = numberOfUsers;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+
 		return false;
 	}
-	
+
 	/**
-	*Takes in a partial solution and includes an additional step.
-	*@param partial Partial solution
-	*@return A partial solution with additional step, or null if no step can be added
-	*/
+	 **Takes in a partial solution and includes an additional step.
+	 **@param partial Partial solution
+	 *	*@return A partial solution with additional step, or null if no step can be added
+	 * */
 	public static int[] extend(int[] partial){
-	
-		return new int[10];
+		int[] temp = new int[friends[0].length];
+		for (int i=0; i<friends[0].length-1; i++){
+			if (partial[i] == 0){
+				temp[i] = 1;
+				return temp;
+			} else {
+				temp[i] = partial[i] ;
+				return temp;			
+			}
+		}
+		return null;
 	}
-	
+
 	/**
-	*The most recent step to be added has been changed to its next option
-	*@param partial Partial solution
-	*@return A partial solution with the most recent step to be added has been changed to its next option
-	*/
+	 **The most recent step to be added has been changed to its next option
+	 **@param partial Partial solution
+	 **@return A partial solution with the most recent step to be added has been changed to its next option
+	 **/
 	public static int[] next(int[] partial){
-	
-		return new int[10];
+		int i = 0;
+		while(i<friends[0].length){
+			if (partial[i+1] == 0){
+				partial[i]= partial[i] + 1;
+				i = friends[0].length;
+			}
+			i++;
+		}
+		return partial;
 	}
-	
 	
 	/**
 	*Tests isFullSolution method using several partial solutions.
@@ -91,9 +128,6 @@ public class FriendsCoupon{
 	public static void main(String[] args){
        	
 		if(args.length == 2) {
-	        
-			System.out.println(args[0]);
-	        System.out.println(args[1]);
 			
 			File inFile = new File(args[0]);
 			Scanner in = new Scanner(System.in);
